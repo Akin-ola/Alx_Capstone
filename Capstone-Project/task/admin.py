a7d32dea62c42d54eb564babb68edce17261a4ba
Akin-ola <idris_akinola@yahoo.com>
2025-03-25 13:16:08 +0100

    Define maintenance model with relationship to technicians and equipment modified

--- Capstone-Project/task/admin.py
+++ Capstone-Project/task/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Equipment, Maintenance, Technician
 
-# Register your models here.
+@admin.register(Equipment)
+class EquipmentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'serial_no', 'svc_status']
+    list_filter = ['name']
+    search_fields = ['name', 'part_no']
+
+@admin.register(Technician)
+class TechnicianAdmin(admin.ModelAdmin):
+    list_display = ['rank', 'name']
+    search_fields = ['name']
+
+@admin.register(Maintenance)
+class MaintenanceAdmin(admin.ModelAdmin):
+    list_display = ['equipment_name', 'category', 'task_date']
+    list_filter = ['equipment_name', 'category', 'task_date']
+    search_fields = ['equipment_name', 'technician' 'category', 'task_date']
--- Capstone-Project/task/models.py
+++ Capstone-Project/task/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-status_options = {'S': 'serviceable', 'U/S': 'unserviceable'}
+status_options = {'serviceable':'S', 'unserviceable':'U/S'}
 
 class Equipment(models.Model):
     name = models.CharField(max_length=200, blank=False)
     part_no = models.CharField(max_length=200, blank=False)
     serial_no = models.CharField(max_length=50, unique=True, blank=False)
     svc_status = models.CharField(max_length=50, choices = status_options, blank=False)
+    remark = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return f"{self.name}"
@@ -18,25 +19,28 @@ class Equipment(models.Model):
 class Technician(models.Model):
     rank = models.CharField(max_length=15)
     name = models.OneToOneField(User, on_delete=models.CASCADE)
+    email = models.EmailField(null=True, blank=False, max_length=100)
     profile_picture = models.ImageField()
 
     def __str__(self):
         return f"{self.name}"
     
-category = {
+maint_category = {
     'periodic': 'Periodic',
-    'preventive': 'preventive',
+    'preventive': 'Preventive',
     'category A': 'Category A',
     'category B': 'Category B',
     'category C': 'Category C',
     'category D': 'Category D'
 }
 class Maintenance(models.Model):
-    equipment_name = models.ForeignKey(Equipment.name, on_delete=models.PROTECT, related_name='task_record')
-    equipment_serial_no = models.ForeignKey(Equipment.serial_no, on_delete=models.PROTECT, related_name='task_record')
+    equipment_name = models.ForeignKey(Equipment, on_delete=models.PROTECT, related_name='task_record')
+    equipment_serial_no = models.ForeignKey(Equipment, on_delete=models.PROTECT)
     technician = models.ForeignKey(Technician, on_delete=models.PROTECT, related_name='task_record')
-    category = models.CharField(max_length=50, choices=category, blank=False)
-    task_desc = models.CharField(max_length=255, blank=False)
+    category = models.CharField(max_length=50, choices=maint_category, blank=False)
+    task_description = models.CharField(max_length=255, blank=False)
     task_date = models.DateField(auto_now_add=True, blank=False)
     remarks = models.CharField(max_length=255, blank=False)
-    
+
+    def __str__(self):
+        return f"{self.category} maintenance on {self.equipment_name}, on {self.task_date}."
